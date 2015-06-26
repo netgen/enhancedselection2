4fdf27d1809da8106f6e471d4c93fc3511ca3c78
Edi Modrić <edi@netgen.hr>
2015-06-26 14:31:53 +0200

    Move copyright notice to root extension folder

--- datatypes/sckenhancedselection/sckenhancedselectiontype.php
+++ datatypes/sckenhancedselection/sckenhancedselectiontype.php
@@ -1,29 +1,5 @@
 <?php
 
-/*
-    Enhanced Selection extension for eZ publish 4.x
-    Copyright (C) 2003-2008  SCK-CEN (Belgian Nuclear Research Centre)
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-*/
-
-/*!
-  \class   SckEnhancedSelectionType sckenhancedselectiontype.php
-  \ingroup eZDatatype
-  \brief   Handles the datatype sckenhancedselection.
-  \version 3.0
-  \date    Tuesday 16 August 2005 9:56:00 am
-  \author  Original Hans Melis, ported to PHP5 by Tom Couwberghs
-*/
-
 class SckEnhancedSelectionType extends eZDataType
 {
     const DATATYPE_STRING = 'sckenhancedselection';
